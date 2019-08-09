1d98d8ff86d2daf251ed7f77968176c7b2ea0720
Kyle Duffy <kyleduffy83@gmail.com>
2019-08-08 22:57:59 -0400

    Revert "Revert "Revert "contractor bug fix again"""
    This reverts commit 44828d9bf42a14d637971205d554370cd2dc4eba.

--- routes/contractors.js
+++ routes/contractors.js
@@ -173,26 +173,16 @@ let contractorsinsurance;
          res.render('error');
          return;
       }
-    try{let insuranceLocal = contractor.insurance.toISOString().substr(0, 10);}  catch(err){}
-      try{let wsibExpLocal = contractor.wsibExp.toISOString().substr(0, 10);}  catch(err){}
-        try{let cafLocal = contractor.caf.toISOString().substr(0, 10);} catch(err){}
+      let insuranceLocal = contractor.insurance.toISOString().substr(0, 10);
+        let wsibExpLocal = contractor.wsibExp.toISOString().substr(0, 10);
+        let cafLocal = contractor.caf.toISOString().substr(0, 10);
   console.log(contractor.services + contractor.services.length);
   console.log(contractor.wsibExp +"                         "+ new Date());
       res.render('contractors/edit', {
       contractor : contractor,
-    if(insuranceLocal)
-    {
-      insurance : insuranceLocal
-    },
-
-
-    if(wsibExpLocal){
-        wsibExp : wsibExpLocal
-    },
-    if(cafLocal){
-        caf : cafLocal
-    },
-
+      insurance : insuranceLocal,
+      wsibExp : wsibExpLocal,
+      caf : cafLocal,
          title: 'Edit Contractor' ,
           user: req.user
       });
